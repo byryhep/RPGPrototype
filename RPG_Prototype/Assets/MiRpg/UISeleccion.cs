f22e0c3a0a66152378528587954e803fd91d81c2
byry <byryhep99@gmail.com>
2019-10-18 12:31:41 +0200

    0.1

---
+++ RPG_Prototype/Assets/MiRpg/UISeleccion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISeleccion : MonoBehaviour
+{
+    public GameObject panelExaminar;
+    public Text txtNombre;
+    public Text txtDescripcion;
+    playerExaminatorSensor sensor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sensor = FindObjectOfType<playerExaminatorSensor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        panelExaminar.SetActive(false);
+        if(sensor)
+        {
+            if (sensor.target)
+            {
+                examinable script = sensor.target.GetComponent<examinable>();
+                if (script)
+                {
+                    txtNombre.text = script.nombre;
+                    txtDescripcion.text = script.descripcion;
+                    panelExaminar.SetActive(true);
+                }
+            }
+        }
+    }
+}
---
+++ RPG_Prototype/Assets/MiRpg/playerExaminatorSensor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerExaminatorSensor : MonoBehaviour
+{
+    public GameObject target;
+    [Tooltip("Distancia a la que se pueden examinar los objetos")]
+    public float examinationMaxDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.SphereCast(transform.position,0.3f,transform.forward,out hit,examinationMaxDistance))
+        {
+            target=hit.collider.gameObject;
+        }
+        else
+            target = null;
+
+        
+    }
+}
